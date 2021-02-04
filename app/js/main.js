ee8910efbe1e4b1cb1b9b9de1093eee1b8f0f7de
Dmitriy <svsdima1995@mail.ru>
2021-02-04 18:57:07 +0300

    Шапка с анимацией

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,18 @@
 $(function(){
 
+$('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="arrows-left"></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="arrows-right"></img>',
+    asNavFor: '.slider-dots',
+});
 
-
+$('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
+    arrows: false,
+});
   
 });
