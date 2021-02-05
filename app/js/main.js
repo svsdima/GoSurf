8299818cea4fdca77f398474c72538714f99c90e
Dmitriy <svsdima1995@mail.ru>
2021-02-05 13:31:56 +0300

    Страница Surf, со слайдом и картой

--- app/js/main.js
+++ app/js/main.js
@@ -14,5 +14,21 @@ $('.slider-dots').slick({
     asNavFor: '.header__slider',
     arrows: false,
 });
+
+$('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="arrows-left"></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="arrows-right"></img>',
+    asNavFor: '.slider-map',
+});
+
+$('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.surf-slider',
+    focusOnSelect: true,
+});
   
 });
